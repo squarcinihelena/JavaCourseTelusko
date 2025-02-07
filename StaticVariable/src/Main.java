6edd8ce6240a5f24770e0fd06546e5ad7e7c1c4f
Helena <107894837+squarcinihelena@users.noreply.github.com>
2025-02-07 20:18:00 -0300

    Add files via upload

---
+++ StaticVariable/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Mobile obj1=new Mobile();
+        obj1.brand="Apple";
+        obj1.price=1500;
+        //obj1.name="SmartPhone";
+        Mobile.name="SmartPhone";
+
+        Mobile obj2=new Mobile();
+        obj2.brand="Samsung";
+        obj2.price=1700;
+        //obj2.name="SmartPhone";
+        Mobile.name="SmartPhone";
+
+        //obj1.name="Phone";
+        Mobile.name="SmartPhone";
+
+        obj1.show();
+        obj2.show();
+
+        //System.out.println(obj1.brand);
+
+
+    }
+}
---
+++ StaticVariable/src/Mobile.java
@@ -0,0 +1,14 @@
+public class Mobile{
+
+    String brand;
+    int price;
+    String network;
+    //	String name;
+
+    static String name;
+
+    public void show() {
+        System.out.println(brand+" : "+price+" : "+name);
+    }
+
+}
